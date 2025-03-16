e595a674f84404560f86d0473659947ec0702dae
NiyiHub <niyiblack@outlook.com>
2025-03-16 15:33:32 +0100

    Ran migration and modified models.py file in the fact checking app.

--- dailynews_backend/settings.py
+++ dailynews_backend/settings.py
@@ -97,18 +97,17 @@ WSGI_APPLICATION = 'dailynews_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-      'default': dj_database_url.config(
-          default=os.environ.get('DATABASE_URL')
-      )
-  }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# DATABASES = {
+#       'default': dj_database_url.config(default=config('DATABASE_URL')
+#       )
+#   }
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- fact_checking/models.py
+++ fact_checking/models.py
@@ -7,7 +7,7 @@ class FactCheckResult(models.Model):
     claim = models.TextField()  # The claim or content being fact checked
     verification_score = models.FloatField(default=0.0)  # Composite score computed from factors
     textual_rating = models.CharField(max_length=50, blank=True)  # E.g., "TRUE", "FALSE", etc.
-    evidence = models.JSONField(null=True, blank=True)  # Stores evidence details (e.g., URLs, excerpts)
+    evidence = models.JSONField(default=dict, null=True, blank=True)  # Stores evidence details (e.g., URLs, excerpts)
     created_at = models.DateTimeField(auto_now_add=True)  # Timestamp for when the fact check was performed
 
     def __str__(self):
